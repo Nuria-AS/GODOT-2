061568b9514d9526d9fb4e928952408249230672
Nuria-AS <nuria.auset2000@gmail.com>
2020-05-29 14:57:44 +0200

    joc_acabat

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -3,13 +3,18 @@ extends KinematicBody2D
 const SPEED = 500
 
 export(PackedScene) var projectile
+export var health = 100
 
 onready var sprite = $Sprite
+onready var timer = $Timer
+onready var death_timer = $DeathTimer
+onready var audio = $Audio
 
 
 var screen_size
 var half_sprite_size
-
+var can_shoot = true
+var dead = false
 
 func _ready():
 	screen_size = get_viewport_rect(). size.x
@@ -21,8 +26,30 @@ func _process(delta):
 	elif Input. is_action_pressed("right"):
 		position.x += SPEED * delta
 	
-	if Input. is_action_pressed("shoot"):
+	if can_shoot and Input. is_action_pressed("shoot"):
+		can_shoot = false
 		var new_projectile = projectile.instance()
-		add_child(new_projectile)
+		get_parent().add_child(new_projectile)  #1.aixo fa que els trets no segueixin la linia de la posicio del player
+		new_projectile.position = position      #2.position es la posicio del player, i la new... es del projectil, aixi k el projectil surt de la nau (player)
+		timer.start()
+		audio.play() #every time he shoots emits a sound - audio
 	
 	position.x = clamp(position.x, 0 + half_sprite_size, screen_size - half_sprite_size)
+
+func _on_Timer_timeout():
+	can_shoot = true
+
+func add_damage(damage):
+	if dead:
+		return
+	health -= damage
+	if health <= 0:
+		dead = true
+		health = 0
+		death_timer.start()
+		set_process(false)
+		sprite.queue_free()
+
+
+func _on_DeathTimer_timeout():
+	get_tree().reload_current_scene()
